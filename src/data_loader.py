45bbb7e95590d6199324ae491ff81d3eef20d832
Divesh <113240252+d-hackmt@users.noreply.github.com>
2025-12-26 11:40:02 +0530

    first commit

---
+++ src/data_loader.py
@@ -0,0 +1,20 @@
+import pandas as pd
+from src.utils import print_header
+
+def load_dataset(path: str) -> pd.DataFrame:
+    """Load dataset from a CSV file.
+    Args:
+        path (str): Path to the CSV file.
+    """
+    print_header("LOADING DATASET")
+    df = pd.read_csv(path)
+    print(f"Dataset loaded with shape: {df.shape}")
+    return df
+
+# TRY LOADING DATA
+
+# if __name__ == "__main__":
+#     df = load_dataset("data/phising.csv")
+#     print("LOADING DATA")
+#     print(df.head())
+    
